e0d25ff4ee36eef1f2a6f506f66684c7cc08b443
guoyongfeng <296512521@qq.com>
2016-06-08 13:01:47 +0800

    task原理

--- tasks.js
+++ tasks.js
@@ -8,13 +8,24 @@ gulp.task  = function(fn){
 
 //开始执行任务
 gulp.start = function(){
-
+    //这个写法不能保证前一个任务执行完毕之后再执行下一个任务
+    /*for(var i=0;i<this.tasks.length;i++){
+        this.tasks[i]();
+    }*/
+    var count = 0;
+    function next(){
+        gulp.tasks[count++](next);
+    }
+    next();
 }
 
-gulp.task(function(){
-    console.log(1);
+gulp.task(function(next){
+    setTimeout(function(){
+        console.log(1);
+        next();
+    },3000);
 });
-gulp.task(function(){
+gulp.task(function(next){
     console.log(2);
 });
 gulp.start();
