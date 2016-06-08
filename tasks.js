206f824c8ec82c88ae9c9b999ef55f604b847cce
guoyongfeng <296512521@qq.com>
2016-06-08 13:05:33 +0800

    task原理

--- tasks.js
+++ tasks.js
@@ -1,9 +1,9 @@
 var gulp = {
-    tasks:[]
+    tasks:[]//定认一个任务的数组
 }
 //添加一个任务
 gulp.task  = function(fn){
-    this.tasks.push(fn);
+    gulp.tasks.push(fn);
 }
 
 //开始执行任务
@@ -12,9 +12,9 @@ gulp.start = function(){
     /*for(var i=0;i<this.tasks.length;i++){
         this.tasks[i]();
     }*/
-    var count = 0;
+    var count = 0;// 表示数组中的第一个索引，也就是表示第一个任务
     function next(){
-        gulp.tasks[count++](next);
+        gulp.tasks[count++](next);//一个next只执行一个函数，如果不再调用next就不会再继续往下执行了
     }
     next();
 }
@@ -22,7 +22,7 @@ gulp.start = function(){
 gulp.task(function(next){
     setTimeout(function(){
         console.log(1);
-        next();
+        //next();
     },3000);
 });
 gulp.task(function(next){
