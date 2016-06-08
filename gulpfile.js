df48d7e018b0bfea4be38f6afcc8e7f11ab97102
guoyongfeng <296512521@qq.com>
2016-06-08 12:01:09 +0800

    copy

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');//引入gulp模块
-//定义一个任务
-//名字叫hello
+/**
+ * 定义一个任务
+ * 把src/index.html拷贝到build目录下
+ * 1. src 获取到处理的文件或目录
+ * 2. dest 拷贝到保存到的目标文件夹
+ */
 gulp.task('hello',function(){
-    console.log('hello');
+    gulp.src('./src/index.html')
+        .pipe(gulp.dest('./build/index.html'))
+    //目标路径是一个文件夹的名字
 });
---
+++ history/1.gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp');//引入gulp模块
+//定义一个任务
+//名字叫hello 第二个参数是任务的定义
+gulp.task('hello',function(){
+    console.log('hello');
+});
