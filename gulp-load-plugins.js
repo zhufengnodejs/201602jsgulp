6d675b2c4d16796573d21d71a2160c50e98e91a8
guoyongfeng <296512521@qq.com>
2016-06-08 18:30:29 +0800

    加载所有的模块

--- gulp-load-plugins.js
+++ gulp-load-plugins.js
@@ -1,10 +1,22 @@
 module.exports = function(){
-    var $ = {};
     /**
      * 1. 读取package.json文件
      * 2. 得到devDependencies配置项
      * 3. 遍历所有的属性，找出所有以gulp-开头的属性
      * 4. 加载这些插件模块并且把返回赋给$的属性(属性等于模块名去掉gulp-前缀)
      */
-    return $;
+    var fs=require('fs');
+    fs.readFile('./package.json','utf8',function(err,data){
+        var $={};
+        var packObj=JSON.parse(data);
+        var DevObj=packObj.devDependencies;
+        var reg=/^gulp-([a-zA-Z-]+)/;
+        for(var key in DevObj){
+            if(reg.test(key)){
+                $[reg.exec(key)[1]]=require(''+key+'')
+            }
+        }
+        return $;
+    });
 }
+
